971d8a0064aa2e128451699365aa81bd0309cf58
Mo Silvera <mo.leslie.silvera@gmail.com>
2019-02-11 15:27:16 -0600

    added edit function

--- src/scripts/contactCollection.js
+++ src/scripts/contactCollection.js
@@ -15,12 +15,21 @@ const contactCollection = {
     //gets the contact array and parses the json
     get: function () {
         return fetch("http://localhost:8088/contacts")
-            .then(response => response.json()
-            )
+            .then(response => response.json() )
+    },
+    //posts to the database - the function accepts an object as the argument
+    Put: function (id, contact) {
+        return fetch(`http://localhost:8088/contacts/${id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(contact)
+        })
     },
     //posts to the database - the function accepts an object as the argument
     Post: function (entryToPost) {
-        fetch("http://localhost:8088/contacts", {
+        return fetch("http://localhost:8088/contacts", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,15 +8,3 @@ import contactChange from "./contactChange"
 contactForm()
 contactList()
 contactChange()
-
-//button to let user choose what is to be edited
-
-//Identify the object to change
-
-//provide a way for user to change the data
-
-//change to save button
-
-// collect new data
-
-//put to api
